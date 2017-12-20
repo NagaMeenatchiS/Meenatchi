02655b9230241a8d712029c45ab1514898d3faf9
NagaMeenatchiS <32257377+NagaMeenatchiS@users.noreply.github.com>
2017-12-20 23:01:23 +0530

    Update digit.cpp

--- digit.cpp
+++ digit.cpp
@@ -5,11 +5,18 @@ int main()
 {
     int n,i=0;
     cin>>n;
+    if(n!=0)
+    {
     while(n!=0)
     {
         n=n/10;
         i++;
     }
+    }
+    else 
+{
+    cout<<"input is zero\n";
+}
     cout<<"the no of digits:\t"<<i;
 
 		return 0;
