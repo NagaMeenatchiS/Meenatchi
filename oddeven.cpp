2fdafe1d49fd04ccf50600b448aef5d9b3c02ee5
NagaMeenatchiS <32257377+NagaMeenatchiS@users.noreply.github.com>
2017-12-20 21:36:46 +0530

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int no;
+	cin>>no;
+	if (no%2==0)
+	{
+	    cout<<"its even\t"<<no;
+	}
+	else
+	{
+	    cout<<"its  odd\t"<<no;
+	}
+	return 0;
+}
