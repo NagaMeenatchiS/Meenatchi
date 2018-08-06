7d142002ac63c4c1561cd6dc3b766cc13c9f7608
NagaMeenatchiS <32257377+NagaMeenatchiS@users.noreply.github.com>
2018-08-06 23:02:54 +0530

    Create Even_interval.java

---
+++ Even_interval.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Even_interval
+{
+    public static void main(String arg[])
+    {
+        int a,b;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        for (int i=a;i<=b;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+            
+        }
+      
+    }
+}
+
+
